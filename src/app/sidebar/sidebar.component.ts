6ab25bea50bd2c03ccbd6b6e25abbdce70c469dc
Erdenedalai Erdenebat <erdenedalai.er@novelsoft.mn>
2023-04-29 15:53:57 +0800

    fix

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from '../services/user-service.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent implements OnInit {
+  imagePath: any;
+  name: any;
 
-  constructor(private userService: UserServiceService) {
-
+  constructor(private userService: UserServiceService,
+    private domSanitizer: DomSanitizer) {
   }
   ngOnInit(): void {
-    this.userService.findOne("644cb46ace8b59cb2b0f1844").subscribe(
+    this.userService.findOne("644cc7389206578cd161bf02").subscribe(
       (val: any) => {
-        console.log(val)
+        console.log(val);
+        if (val.data) {
+          const data = val.data;
+          this.imagePath = this.domSanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,'
+            + data.picture);
+          this.name = data.fname;
+        }
       }
     );
   }
